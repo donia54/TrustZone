4ed335b1e67a431a2230d8c1827ee74e343215ee
donia <arefdonia491@gmail.com>
2025-04-18 22:45:59 +0200

    user profile controlller using current user 1

--- TrustZoneAPI/Controllers/Users/UserProfileController.cs
+++ TrustZoneAPI/Controllers/Users/UserProfileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TrustZoneAPI.DTOs.Users;
 using TrustZoneAPI.Services.Users;
@@ -11,79 +12,89 @@ namespace TrustZoneAPI.Controllers.Users
     {
 
         private readonly IUserProfileService _userProfileService;
-
+        
         public UserProfileController(IUserProfileService userProfileService)
         {
             _userProfileService = userProfileService;
         }
 
-        [HttpGet("{userId}")]
-        public async Task<ActionResult> GetUserProfile(string userId)
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> GetUserProfile()
         {
-            var response = await _userProfileService.GetUserProfileAsync(userId);
-            return MapResponseToActionResult(response);
-        }
-
-      
-        [HttpPut("updateInfo/{userId}")]
-        public async Task<ActionResult> UpdateUserInfo(string userId, [FromBody] UpdateUserProfileDTO dto)
-        {
-            var response = await _userProfileService.UpdateUserInfoAsync(userId, dto);
-            return MapResponseToActionResult(response);
-        }
-
-      
-        [HttpPut("updatePassword/{userId}")]
-        public async Task<ActionResult> UpdatePassword(string userId, [FromBody] UpdatePasswordDTO dto)
-        {
-            var response = await _userProfileService.UpdatePasswordAsync(userId, dto);
-            return MapResponseToActionResult(response);
-        }
-
-        
-        [HttpGet("generateProfilePictureUploadSas/{userId}/{fileName}")]
-        public async Task<ActionResult> GenerateProfilePictureUploadSasUrl(string userId, string fileName)
-        {
-            var response = await _userProfileService.GenerateProfilePictureUploadSasUrlAsync(userId, fileName);
-            return MapResponseToActionResult(response);
-        }
-
-      
-        [HttpGet("generateCoverPictureUploadSas/{userId}/{fileName}")]
-        public async Task<ActionResult> GenerateCoverPictureUploadSasUrl(string userId, string fileName)
-        {
-            var response = await _userProfileService.GenerateCoverPictureUploadSasUrlAsync(userId, fileName);
-            return MapResponseToActionResult(response);
-        }
-
-        [HttpPut("updateProfilePicture/{userId}/{fileName}")]
-        public async Task<ActionResult> UpdateProfilePicture(string userId, string fileName)
-        {
-            var response = await _userProfileService.UpdateProfilePictureAsync(userId, fileName);
+            var response = await _userProfileService.GetUserProfileAsync(CurrentUserId);
             return MapResponseToActionResult(response);
         }
 
 
-        [HttpPut("updateCoverPicture/{userId}/{fileName}")]
-        public async Task<ActionResult> UpdateCoverPicture(string userId, string fileName)
+        [Authorize]
+        [HttpPut]
+        public async Task<ActionResult> UpdateUserInfo( [FromBody] UpdateUserProfileDTO dto)
         {
-            var response = await _userProfileService.UpdateCoverPictureAsync(userId, fileName);
+            var response = await _userProfileService.UpdateUserInfoAsync(CurrentUserId, dto);
             return MapResponseToActionResult(response);
         }
 
-        
-        [HttpGet("getProfilePictureUrl/{userId}")]
-        public async Task<ActionResult> GetProfilePictureUrl(string userId)
+
+        [Authorize]
+        [HttpPut ("password")]
+        public async Task<ActionResult> UpdatePassword( [FromBody] UpdatePasswordDTO dto)
         {
-            var response = await _userProfileService.GetProfilePictureUrlAsync(userId);
+            var response = await _userProfileService.UpdatePasswordAsync(CurrentUserId, dto);
             return MapResponseToActionResult(response);
         }
 
-        
-        [HttpGet("getCoverPictureUrl/{userId}")]
-        public async Task<ActionResult> GetCoverPictureUrl(string userId)
+
+        [Authorize]
+        [HttpGet("generateProfilePictureUploadSas/{fileName}")]
+        public async Task<ActionResult> GenerateProfilePictureUploadSasUrl( string fileName)
         {
-            var response = await _userProfileService.GetCoverPictureUrlAsync(userId);
+            var response = await _userProfileService.GenerateProfilePictureUploadSasUrlAsync(CurrentUserId, fileName);
+            return MapResponseToActionResult(response);
+        }
+
+
+        [Authorize]
+        [HttpGet("generateCoverPictureUploadSas/{fileName}")]
+        public async Task<ActionResult> GenerateCoverPictureUploadSasUrl( string fileName)
+        {
+            var response = await _userProfileService.GenerateCoverPictureUploadSasUrlAsync(CurrentUserId, fileName);
+            return MapResponseToActionResult(response);
+        }
+
+
+        [Authorize]
+        [HttpPut("ProfilePicture/{fileName}")]
+        public async Task<ActionResult> UpdateProfilePicture(string fileName)
+        {
+            var response = await _userProfileService.UpdateProfilePictureAsync(CurrentUserId, fileName);
+            return MapResponseToActionResult(response);
+        }
+
+
+        [Authorize]
+        [HttpPut("CoverPicture/{fileName}")]
+        public async Task<ActionResult> UpdateCoverPicture( string fileName)
+        {
+            var response = await _userProfileService.UpdateCoverPictureAsync(CurrentUserId, fileName);
+            return MapResponseToActionResult(response);
+        }
+
+
+        [Authorize]
+        [HttpGet("profile-picture")]
+        public async Task<ActionResult> GetProfilePictureUrl()
+        {
+            var response = await _userProfileService.GetProfilePictureUrlAsync(CurrentUserId);
+            return MapResponseToActionResult(response);
+        }
+
+
+        [Authorize]
+        [HttpGet("cover-picture")]
+        public async Task<ActionResult> GetCoverPictureUrl()
+        {
+            var response = await _userProfileService.GetCoverPictureUrlAsync(CurrentUserId);
             return MapResponseToActionResult(response);
         }
     }
