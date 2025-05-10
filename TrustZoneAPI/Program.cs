77d5ee4ce94aaa8586afbe39798be2a047563863
donia <arefdonia491@gmail.com>
2025-05-10 20:35:22 +0300

    commit

--- TrustZoneAPI/Program.cs
+++ TrustZoneAPI/Program.cs
@@ -208,10 +208,12 @@ builder.Services.AddOpenApi();
 //{
 //    options.AddDefaultPolicy(policy =>
 //    {
-//        policy.WithOrigins("http://127.0.0.1:5500",
-//                "http://localhost:5500",
-//                "https://localhost:5500",
-//                "https://trustzone.azurewebsites.net")
+//        policy.WithOrigins("http://127.0.0.1:5500",  // Live Server (VS Code)
+//                "http://localhost:5500",   // Live Server
+//                "https://localhost:5500",  // Live Server (HTTPS)
+//                "http://localhost:3000",  // React/Vite default
+//                "https://trustzone.azurewebsites.net",
+//                "null")
 //              .AllowAnyHeader()
 //              .AllowAnyMethod()
 //        .AllowCredentials();
